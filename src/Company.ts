6e93647afc6090d004249eb9e1a5ac60cfa45c6d
olliebrownlow <olliebrownlow@gmail.com>
2020-06-17 23:22:49 +0100

    adds customised content to the popups by adding function to Mappable interface and ensuring that User and Company implement that function

--- src/Company.ts
+++ src/Company.ts
@@ -16,4 +16,13 @@ export class Company {
       lng: parseFloat(faker.address.longitude()),
     };
   }
+
+  markerContent(): string {
+    return `
+      <div>
+        <h1>User Name: ${this.companyName}</h1>
+        <h3>Catchphrase: ${this.catchPhrase}</h3>
+      </div>
+    `;
+  }
 }
